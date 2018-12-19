78a58e2db069fc91cfcb06a2f95da583a83bd0a3
Ahmed Essam <eng.eramax@gmail.com>
2018-12-19 18:42:10 +0300

    images issue fixed

--- src/Containers/DefaultHeader.js
+++ src/Containers/DefaultHeader.js
@@ -30,8 +30,18 @@ class DefaultHeader extends Component {
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+          full={{
+            src: '../assets/imgs/logo2.svg',
+            width: 89,
+            height: 25,
+            alt: 'CoreUI Logo'
+          }}
+          minimized={{
+            src: '../assets/imgs/smallLogo.svg',
+            width: 30,
+            height: 30,
+            alt: 'CoreUI Logo'
+          }}
         />
         <AppSidebarToggler className="d-md-down-none" display="lg" />
 
